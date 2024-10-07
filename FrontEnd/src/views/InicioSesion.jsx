4ad23db1a5b316b155bb9a4b6e92dfce75fb7215
jcidcab@gmail.com <jcidcab@gmail.com>
2024-10-07 00:13:22 -0300

    resolviendo conflictos

--- FrontEnd/src/views/InicioSesion.jsx
+++ FrontEnd/src/views/InicioSesion.jsx
@@ -1,43 +1,16 @@
 import React from 'react';
-
-import NavbarHome from '../components/NavbarHome';
+import NavbarHome from '../components/NavbarHome'
 import FormularioAcceso from '../components/FormularioAcceso';
 import Footer from '../components/Footer';
-import "../css/InicioSesion.css";
+
 
 function InicioSesion() {
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <div className='InicioSesion'>
-      
-      <div className='navbar'>
-        <NavbarHome />
-      </div>
-
-      {/* Formulario de acceso al centro */}
-      <div className='formulario'>
-        <FormularioAcceso />
-      </div>
-
-      {/* Footer que ocupa el 100% de ancho en la parte inferior */}
-      <div className='footer'>
-        <Footer />
-      </div>
-=======
-    <div className='loginview'>
-=======
     <>
->>>>>>> origin/main
     <NavbarHome/>
     <h1 className='text-center'>Inicio Sesion</h1>
     <div className='loginview ms-5 me-5 mt-5'>
     <FormularioAcceso/>
-<<<<<<< HEAD
-    <Footer/>
->>>>>>> main
-=======
->>>>>>> origin/main
     </div>
     <Footer/>
     </>
