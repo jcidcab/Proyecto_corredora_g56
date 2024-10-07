d631e3d26b9b68d402eab3cfdf65b5efa508798b
jcidcab@gmail.com <jcidcab@gmail.com>
2024-10-06 22:44:04 -0300

    cambio nombre

--- FrontEnd/src/components/NavbarHome.jsx
+++ FrontEnd/src/components/NavbarHome.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import { Navbar, Nav, Button } from 'react-bootstrap';
-import './Navbar.css'; 
+import './Navbar.css';
 import { Link } from 'react-router-dom';
-
 
 function NavbarHome() {
   return (
-    <Navbar bg="light" expand="lg" className='ps-5'>
-      <Navbar.Brand href="#home" className='ms-5'>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNci0l4oy4eruJe5fuhETj2UQ_wI2r6P70qg&s"
-          width="50"
-          height="50"
-          className="d-inline-block align-top"
-          alt="Logo"
-        />
-        {' SuCasa'}
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ms-auto">
-          <Link to="/inicio-sesion">
-            <Button variant="outline-primary" className="me-4 rounded-button">Acceder</Button>
-          </Link>
-          <Link to="/registro">
-            <Button variant="primary" className="me-5 rounded-button">Registro</Button>
-          </Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div className='navbarflex'>
+      <Navbar bg="light" expand="lg" className='navbar-custom'>
+        <Navbar.Brand href="#home">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNci0l4oy4eruJe5fuhETj2UQ_wI2r6P70qg&s"
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+            alt="Logo"
+          />
+          {' SuCasa'}
+        </Navbar.Brand>
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto"> {/* ms-auto empuja el contenido hacia la derecha */}
+            <Link to="/inicio-sesion">
+              <Button variant="outline-primary" className="me-4 rounded-button">Acceder</Button>
+            </Link>
+            <Link to="/registro">
+              <Button variant="primary" className="me-5 rounded-button">Registro</Button>
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   );
 }
 
 export default NavbarHome;
-
-
--- FrontEnd/src/views/InicioSesion.jsx
+++ FrontEnd/src/views/InicioSesion.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import NavbarHome from '../components/NavbarHome'
+
+import NavbarHome from '../components/NavbarHome';
 import FormularioAcceso from '../components/FormularioAcceso';
 import Footer from '../components/Footer';
-
+import "../css/InicioSesion.css";
 
 function InicioSesion() {
   return (
-    <>
-    <NavbarHome/>
-    <FormularioAcceso/>
-    <Footer/>
-    </>
+    <div className='InicioSesion'>
+      
+      <div className='navbar'>
+        <NavbarHome />
+      </div>
+
+      {/* Formulario de acceso al centro */}
+      <div className='formulario'>
+        <FormularioAcceso />
+      </div>
+
+      {/* Footer que ocupa el 100% de ancho en la parte inferior */}
+      <div className='footer'>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
